186c5b1f1970e1b7571f8e495943ac52d37bab21
Matěj <mckillem@tuta.io>
2023-11-13 08:40:40 +0100

    zobrazení seznamu videí

--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -12,11 +12,18 @@ export const Main = ({url}: {url: string}) => {
 				return <div key={id}>
 					<h1>{title}</h1>
 					<video src={video} width="800" controls={true} autoPlay={false}/>
-					<p>{creator}</p>
+					<h2>{creator}</h2>
 					<p>{description}</p>
 				</div>
 			}) :
-		content = "seznam videí"
+		content = Videos.map(({id, title, creator, video, description}: VideoObject) => {
+			return <div key={id}>
+				<h1>{title}</h1>
+				<video src={video} width="400" controls={true} autoPlay={false}/>
+				<h2>{creator}</h2>
+				<p>{description}</p>
+			</div>
+		})
 
 	return (
 		<div id={"main"}>
