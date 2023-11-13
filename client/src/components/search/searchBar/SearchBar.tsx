34c45ce191393a19879bdc0c2116c3b239bb21ac
Matěj <mckillem@tuta.io>
2023-11-13 09:30:07 +0100

    přidáno vyhledávání dle nýzvu videa

---
+++ client/src/components/search/searchBar/SearchBar.tsx
@@ -0,0 +1,49 @@
+import {ChangeEvent, useState} from "react";
+import "./SearchBar.scss";
+import {VideoObject} from "../../../implementations/VideoObject";
+
+function SearchBar({ placeholder, videos }: {placeholder: string, videos: VideoObject[]}) {
+	const [filteredVideos, setFilteredVideos] = useState<VideoObject[]>([]);
+	const [wordEntered, setWordEntered] = useState<string>("");
+
+	const handleFilter = (event: ChangeEvent<HTMLInputElement>) => {
+		const searchWord: string = event.target.value;
+		setWordEntered(searchWord);
+		const filmsFilter: VideoObject[] = videos.filter((value: VideoObject) => value.title.toLowerCase().includes(searchWord.toLowerCase()));
+
+		if (searchWord === "") {
+			setFilteredVideos([]);
+		} else {
+			setFilteredVideos(filmsFilter);
+		}
+	};
+
+	return (
+		<div className="search">
+			<div className="searchInputs">
+				<input
+					type="text"
+					placeholder={placeholder}
+					value={wordEntered}
+					onChange={handleFilter}
+				/>
+			</div>
+			{filteredVideos.length !== 0 && (
+				<div className="dataResult">
+					<h3>Videa</h3>
+					{filteredVideos.slice(0, 15).map(({id, title}: VideoObject) => {
+						return (
+							<div key={id}>
+								<a className="dataItem" href={"/" + id} target="_self">
+								 	<p>{title}</p>
+								</a>
+							</div>
+						);
+					})}
+				</div>
+			)}
+		</div>
+	);
+}
+
+export default SearchBar;
--- client/src/pages/Header.tsx
+++ client/src/pages/Header.tsx
@@ -1,10 +1,14 @@
 import "./Header.scss";
+import SearchBar from "../components/search/searchBar/SearchBar";
+import {Videos} from "../mockData/Videos";
 
 export const Header = () => {
 
 	return (
 		<div id={"header"}>
-			<h1>hlava</h1>
+			<div id="searchBar">
+				<SearchBar placeholder="Videa" videos={Videos}></SearchBar>
+			</div>
 		</div>
 	);
 };
