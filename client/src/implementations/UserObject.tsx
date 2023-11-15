424159a0a3d155ea15c9f5acd173b2520dbb22fa
Matěj <mckillem@tuta.io>
2023-11-15 16:11:13 +0100

    uživatel

---
+++ client/src/implementations/UserObject.tsx
@@ -0,0 +1,7 @@
+export interface UserObject {
+	id: number;
+	firstName: string;
+	lastName: string;
+	email: string;
+	channel: number;
+}
---
+++ client/src/mockData/Users.tsx
@@ -0,0 +1,25 @@
+import {UserObject} from "../implementations/UserObject";
+
+export const Users:UserObject[] = [
+	{
+		id: 1,
+		firstName: "Jan",
+		lastName: "Pomuk",
+		email: "email",
+		channel: 1,
+	},
+	{
+		id: 2,
+		firstName: "János",
+		lastName: "Kumop",
+		email: "email",
+		channel: 2,
+	},
+	{
+		id: 3,
+		firstName: "Sofin",
+		lastName: "Ompokupas",
+		email: "email",
+		channel: 3,
+	},
+]
